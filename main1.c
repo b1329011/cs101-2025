a79637208ba45688d30d5474dcc9abedafe7b9c1
b1329011 <lisihyu@gmail.com>
2025-04-10 14:05:19 +0800

    main1.c

--- main1.c
+++ main1.c
@@ -1,18 +1,13 @@
 #include <stdio.h>
 
-int main()
-{
-    int i=3031;
-    int a=i-1500;
-    if(a<=0){
-        printf("70元\n");
-    }else if(a<=100){
-        printf("80元\n");
-    }else{
-        int c=a/100;
-        int r=(a%100)?1:0;
-        int sum=(c+r)*10+70;
-        printf("%d元\n",sum);
-    }
+int factorial(int n) {
+    if (n == 1) return 1;
+    return n * factorial(n - 1);
+}
+
+int main() {
+    int n = 5;
+    int m = factorial(n);
+    printf("factorial %d = %d\n", n, m);
     return 0;
 }
